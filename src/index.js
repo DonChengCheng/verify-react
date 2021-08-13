5e98e525c313fd0ba766d6e3273f9e60b570f0c1
dongchengcheng <dongchengcheng@115.com>
2021-08-13 16:16:25 +0800

    a3

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import SlideVerify from './components/SlideVerify';
 import PointVerfiy from './components/PointVerify';
 import './index.css';
 console.log('i am allen');
+console.log('i am allen1');
+console.log('i am allen2');
+console.log('i am allen3');
 
 export {
     CodeVerify,
